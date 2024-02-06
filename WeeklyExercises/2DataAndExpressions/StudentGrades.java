d78de1a338ed3c211392f7f46235a2d0226c15c8
LGandia <imposiblenormalito@gmail.com>
2024-02-06 14:12:04 +0000

    table created

--- WeeklyExercises/2DataAndExpressions/StudentGrades.java
+++ WeeklyExercises/2DataAndExpressions/StudentGrades.java
@@ -1,7 +1,18 @@
 public class StudentGrades {
     public static void main (String[] args) {
+
         System.out.println("///////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
         System.out.println("==          Student Points          ==");
         System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\///////////////////");
+
+        System.out.println("\nName           Lab     Bonus   Total");
+        System.out.println("----           ---     -----   -----");
+
+        final Object[][] grades = new String[5][];
+        grades[0] = new String[] {"Walt", "50", "7","57"};
+
+        for (final Object[] row : grades){
+            System.out.format("%-15s%-9s%-8s%-9s%n", row);
+        }
     }
 }
