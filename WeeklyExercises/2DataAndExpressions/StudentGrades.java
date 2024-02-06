c9389c887189a9b50c5dd4d713f65edbd8c7a328
LGandia <imposiblenormalito@gmail.com>
2024-02-06 13:38:05 +0000

    printed header

---
+++ WeeklyExercises/2DataAndExpressions/StudentGrades.java
@@ -0,0 +1,10 @@
+public class StudentGrades {
+    //----------------------------------------------------
+    //  Displays a table showing student grades and points
+    //----------------------------------------------------
+    public static void main (String[] args) {
+        System.out.println("///////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+        System.out.println("==          Student Points          ==");
+        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\///////////////////");
+    }
+}
