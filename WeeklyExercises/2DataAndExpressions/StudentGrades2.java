866f4b063c904a608a5ed12fbce5aa361929e21e
LGandia <imposiblenormalito@gmail.com>
2024-02-08 10:50:40 +0000

    alternate solution

---
+++ WeeklyExercises/2DataAndExpressions/StudentGrades2.java
@@ -0,0 +1,19 @@
+public class StudentGrades2 {
+    public static void main (String[] args)
+    {
+
+        System.out.println("///////////////////" +
+                "\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+        System.out.println("==          Student Points          ==");
+        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\" +
+                "///////////////////");
+
+        System.out.println("\nName\t\t\tLab\t\tBonus\tTotal");
+        System.out.println("----\t\t\t---\t\t-----\t-----");
+        System.out.println("Walt" +"\t\t\t" +"70"+"\t\t"+"7"+"\t\t"+(70+7));
+        System.out.println("Jesse"+"\t\t\t"+"25"+"\t\t"+"3"+"\t\t"+(25+3));
+        System.out.println("Gus"+"\t\t\t\t"+"70"+"\t\t"+"30"+"\t\t"+(70+30));
+        System.out.println("Mike"+"\t\t\t"+ "40"+"\t\t"+ "10"+"\t\t"+(40+10));
+        System.out.println("Hank"+"\t\t\t"+ "50"+"\t\t"+"7"+"\t\t"+(50+7));
+    }
+}
