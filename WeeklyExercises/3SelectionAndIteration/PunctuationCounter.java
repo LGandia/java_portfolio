3fbb0ac68b64be8f9c64496e5559605c34e767bb
LGandia <imposiblenormalito@gmail.com>
2024-02-13 15:06:34 +0000

    saving progress

--- WeeklyExercises/3SelectionAndIteration/PunctuationCounter.java
+++ WeeklyExercises/3SelectionAndIteration/PunctuationCounter.java
@@ -1,13 +1,21 @@
-public class PunctuationCounter {
+import java.util.Scanner;
+
+public class PunctuationCounter
+{
     public static void main (String[] args)
     {
         PunctuationCounter O = new PunctuationCounter();
         O.go();
         //https://www.javatpoint.com/program-to-count-all-punctuation-characters-in-the-string
     }
-    public void go(){
-        int counter;
+    public void go()
+    {
+        int counter = 0;
         String sentence;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Input sentence: ");
+        sentence = scan.next();
 
     }
 }
