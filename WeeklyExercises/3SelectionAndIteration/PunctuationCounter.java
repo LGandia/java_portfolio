fecefb57e7b7b2867b4f202f3f24c97a49ce6bd9
LGandia <imposiblenormalito@gmail.com>
2024-02-13 14:55:53 +0000

    possible helpful links

--- WeeklyExercises/3SelectionAndIteration/PunctuationCounter.java
+++ WeeklyExercises/3SelectionAndIteration/PunctuationCounter.java
@@ -1,2 +1,13 @@
 public class PunctuationCounter {
+    public static void main (String[] args)
+    {
+        PunctuationCounter O = new PunctuationCounter();
+        O.go();
+        //https://www.javatpoint.com/program-to-count-all-punctuation-characters-in-the-string
+    }
+    public void go(){
+        int counter;
+        String sentence;
+
+    }
 }
---
+++ WeeklyExercises/3SelectionAndIteration/StringReverse.java
@@ -0,0 +1,7 @@
+public class StringReverse{
+    public static void main(String[] args){
+    //https://www.geeksforgeeks.org/reverse-individual-words/
+    }
+    public void go(){
+    }
+}
