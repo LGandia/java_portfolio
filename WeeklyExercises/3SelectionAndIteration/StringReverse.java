02b2a56b46ef126d37144a37ef00df73f359793b
imposible normalito <imposiblenormalito@gmail.com>
2024-02-19 22:51:03 +0000

    created variable reverse

--- WeeklyExercises/3SelectionAndIteration/StringReverse.java
+++ WeeklyExercises/3SelectionAndIteration/StringReverse.java
@@ -1,7 +1,10 @@
 public class StringReverse{
     public static void main(String[] args){
     //https://www.geeksforgeeks.org/reverse-individual-words/
+        StringReverse O = new StringReverse();
+        O.go();
     }
     public void go(){
+        String reverse = "";
     }
 }
