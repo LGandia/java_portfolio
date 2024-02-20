1bc98f0087a714b21ed2119c94301ac11c007add
LGandia <imposiblenormalito@gmail.com>
2024-02-20 13:25:34 +0000

    created variables

---
+++ WeeklyExercises/4ClassesAndObjects/Dice.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Dice
+{
+
+    public static void main (String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+
+        int[] roll1;
+        System.out.print("How many sides does die 1 have? ");
+        int DieSide1 = scan.nextInt();
+
+        int roll2;
+        System.out.print("\nHow many sides does die 2 have? ");
+        int DieSide2 = scan.nextInt();
+
+    }
+}
