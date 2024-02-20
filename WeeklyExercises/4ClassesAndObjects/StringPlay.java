1a0e1a8636cd2355f56ffb69d5f8c35c4e10d206
imposible normalito <imposiblenormalito@gmail.com>
2024-02-20 09:49:28 +0000

    part a

---
+++ WeeklyExercises/4ClassesAndObjects/StringPlay.java
@@ -0,0 +1,36 @@
+// **************************************************
+//   StringPlay.java
+//
+//   Play with String objects
+// **************************************************
+public class StringPlay
+{
+    public static void main (String[] args)
+    {
+        String college = new String ("Leeds Beckett University"); //Similar for part (a)
+
+        String town = "Anytown, UK"; // part (a)
+
+
+        int stringLength;
+        String change1, change2, change3;
+
+
+        ________________________________________________________; // part (b)
+
+
+        System.out.println (college + " contains " + stringLength + " characters.");
+
+
+        change1 = ______________________________________________; // part (c)
+
+
+        change2 = ______________________________________________; // part (d)
+
+
+        change3 = ______________________________________________; // part (e)
+
+
+        System.out.println ("The final string is " + change3);
+    }
+}
