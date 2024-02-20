f411f0b57c43eb9a9eb8873e9c5ba4b98d9b73a8
imposible normalito <imposiblenormalito@gmail.com>
2024-02-20 09:21:28 +0000

    fixed yellow warning through IntelliJ

--- WeeklyExercises/3SelectionAndIteration/StringReverse.java
+++ WeeklyExercises/3SelectionAndIteration/StringReverse.java
@@ -10,13 +10,13 @@ public class StringReverse{
         Scanner scan = new Scanner(System.in);
 
         String sentence;
-        String reverse = "";
+        StringBuilder reverse = new StringBuilder();
 
         System.out.print("Input the word(s) to be reversed: ");
         sentence = scan.nextLine();
 
         for (int i = sentence.length() - 1; i>=0; i--){
-            reverse += sentence.charAt(i);
+            reverse.append(sentence.charAt(i));
         }
         System.out.println(sentence.length());
         System.out.println(reverse);
