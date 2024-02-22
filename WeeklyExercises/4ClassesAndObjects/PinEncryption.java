386b5bcdf891ab2f225e0cea1efab48bbf94ff83
LGandia <imposiblenormalito@gmail.com>
2024-02-22 10:08:34 +0000

    program working

--- WeeklyExercises/4ClassesAndObjects/PinEncryption.java
+++ WeeklyExercises/4ClassesAndObjects/PinEncryption.java
@@ -7,13 +7,15 @@ public class PinEncryption {
         Scanner scan = new Scanner(System.in);
         Random random = new Random();
 
-        System.out.println("Enter 4 digit number to encrypt: ");
+        System.out.print("Enter 4 digit number to encrypt: ");
         int pin = scan.nextInt();
-        String hexpin = Integer.toHexString(pin);
+        String pin_hex = Integer.toHexString(pin);
 
         int number1 = random.nextInt(1000, 655336);
         String hex1 = Integer.toHexString(number1);
         int number2 = random.nextInt(1000, 655336);
         String hex2 = Integer.toHexString(number2);
+
+        System.out.println("Your encrypted pin is: "+hex1+pin_hex+hex2);
     }
 }
