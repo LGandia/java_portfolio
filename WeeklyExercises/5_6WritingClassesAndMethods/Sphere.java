3babafdb04dea5519553ff06f7f7a29646ab4166
LGandia <imposiblenormalito@gmail.com>
2024-02-27 14:18:59 +0000

    constructor updated

--- WeeklyExercises/5_6WritingClassesAndMethods/Sphere.java
+++ WeeklyExercises/5_6WritingClassesAndMethods/Sphere.java
@@ -2,18 +2,20 @@ class Sphere
 {
     private double diameter, volume, surfaceArea;
 
-    // Diameter constructor, getter and setter methods.
-    public Sphere(double diameter) {
+    // constructor method
+    public Sphere(double diameter, double volume, double surfaceArea) {
         this.diameter = diameter;
+        this.volume = ((double) 4 /3)*( (double) 22 /7)*(diameter/2)*(diameter/2)*(diameter/2);
+        this.surfaceArea = (double) (4 * (22/7))*diameter*diameter;
     }
+
+    // Diameter getter and setter methods.
     public double getDiameter() {
         return diameter;
     }
     public void setDiameter(double diameter) {
         this.diameter = diameter;
     }
-    // Volume
-
 }
 
 
