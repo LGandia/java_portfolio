06430838bca7741c68759c3f841e80074d9b85c8
LGandia <imposiblenormalito@gmail.com>
2024-02-27 14:01:38 +0000

    started task 1

---
+++ WeeklyExercises/5_6WritingClassesAndMethods/Sphere.java
@@ -0,0 +1,20 @@
+class Sphere
+{
+    private double diameter, volume, surfaceArea;
+
+    // Diameter constructor, getter and setter methods.
+    public Sphere(double diameter) {
+        this.diameter = diameter;
+    }
+    public double getDiameter() {
+        return diameter;
+    }
+    public void setDiameter(double diameter) {
+        this.diameter = diameter;
+    }
+    // Volume
+
+}
+
+
+
