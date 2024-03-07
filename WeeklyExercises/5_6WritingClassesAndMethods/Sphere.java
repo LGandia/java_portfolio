fc23b4f3866756a18032eff058c38348b64793ca
imposible normalito <imposiblenormalito@gmail.com>
2024-03-07 15:29:44 +0000

    update readme

--- WeeklyExercises/5_6WritingClassesAndMethods/Sphere.java
+++ WeeklyExercises/5_6WritingClassesAndMethods/Sphere.java
@@ -1,43 +1,5 @@
-public class Sphere
-{
-    private double diameter;
-    private final double volume;
-    private final double surfaceArea;
-
-    // constructor method
-    public Sphere(double diameter, double volume, double surfaceArea) {
-        this.diameter = diameter;
-        this.volume = ((double) 4 /3)*( (double) 22 /7)*(diameter/2)*(diameter/2)*(diameter/2);
-        this.surfaceArea = (double) (4 * (22/7))*diameter*diameter;
-    }
-
-    // Diameter getter and setter methods.
-    public double getDiameter() {
-        return diameter;
-    }
-    public void setDiameter(double diameter) {
-        this.diameter = diameter;
-    }
-
-    public double getVolume() {
-        return volume;
-    }
-
-    public double getSurfaceArea() {
-        return surfaceArea;
-    }
-
-    @Override
-    public String toString() {
-        return "Sphere{" +
-                "diameter=" + diameter +
-                ", volume=" + volume +
-                ", surfaceArea=" + surfaceArea +
-                '}';
-    }
-    public static void main(String[] args) {
-    }
+public class Sphere {
+        double diameter;
+        double volume = (4*((double) 22 /7)*Math.pow((diameter/2),3))/3;
+        double surfaceArea = 4*((double) 22 /7)*Math.pow((diameter/2),2);
 }
-
-
-
