c504e2d1d05134e3afbb1cf6e8339c76593956e9
imposible normalito <imposiblenormalito@gmail.com>
2024-03-13 10:30:31 +0000

    created cleaner,receptionist and nurse classes

--- WeeklyExercises/7Inheritance/Hospital.java
+++ WeeklyExercises/7Inheritance/Hospital.java
@@ -1,4 +1,4 @@
-class Employee{
+abstract class Employee{
     public int employeeID;
     public String employeeName;
     public String employeeDepartment;
@@ -26,9 +26,49 @@ class Doctor extends Employee{
         System.out.println("Work: "+this.employeeWorkType);
     }
 }
-// class nurse extends employee{}
-// class receptionist extends employee{}
-// class cleaner extends employee{}
+class Nurse extends Employee{
+    public String employeeDuties;
+    public Nurse(int employeeID, String employeeName, String employeeDepartment, String employeeWorkType, String employeeDuties) {
+        super(employeeID, employeeName, employeeDepartment, employeeWorkType);
+        this.employeeDuties = employeeDuties;
+    }
+
+    public void getNurseDetails() {
+        System.out.println("Employee ID: "+this.employeeID);
+        System.out.println("Name: "+this.employeeName);
+        System.out.println("Department: "+this.employeeDepartment);
+        System.out.println("Work: "+this.employeeWorkType);
+    }
+}
+
+class Receptionist extends Employee{
+    public String employeeDuties;
+    public Receptionist(int employeeID, String employeeName, String employeeDepartment, String employeeWorkType, String employeeDuties) {
+        super(employeeID, employeeName, employeeDepartment, employeeWorkType);
+        this.employeeDuties = employeeDuties;
+    }
+
+    public void getReceptionistDetails() {
+        System.out.println("Employee ID: "+this.employeeID);
+        System.out.println("Name: "+this.employeeName);
+        System.out.println("Department: "+this.employeeDepartment);
+        System.out.println("Work: "+this.employeeWorkType);
+    }
+}
+class Cleaner extends Employee{
+    public String employeeDuties;
+    public Cleaner(int employeeID, String employeeName, String employeeDepartment, String employeeWorkType, String employeeDuties) {
+        super(employeeID, employeeName, employeeDepartment, employeeWorkType);
+        this.employeeDuties = employeeDuties;
+    }
+
+    public void getCleanerDetails() {
+        System.out.println("Employee ID: "+this.employeeID);
+        System.out.println("Name: "+this.employeeName);
+        System.out.println("Department: "+this.employeeDepartment);
+        System.out.println("Work: "+this.employeeWorkType);
+    }
+}
 class Hospital {
     public static void main (String[] args) {
         Doctor doctor = new Doctor(498984, "Dylan", "Medicine","Cardiologist","Work");
