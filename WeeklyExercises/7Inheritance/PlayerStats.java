0b2bddfa8360ff250a6ebad3cf3ad3059f4dc19c
imposible normalito <imposiblenormalito@gmail.com>
2024-03-15 13:32:06 +0000

    footballer details added

--- WeeklyExercises/7Inheritance/PlayerStats.java
+++ WeeklyExercises/7Inheritance/PlayerStats.java
@@ -12,25 +12,39 @@ abstract class PlayerStats{
     }
 }
 class FootballStats extends PlayerStats{
-    public int goalsScored;
     public String league;
+    public int goalsScored;
+    public int shirtNumber;
     public String fieldPosition;
     public FootballStats(String playerName, String playerSurname, String playerSport, String playerTeam,
-                         int goalsScored, String league, String fieldPosition) {
+                         int goalsScored, int shirtNumber, String league, String fieldPosition) {
         super(playerName, playerSurname, playerSport, playerTeam);
 
-        this.goalsScored = goalsScored;
         this.league = league;
+        this.goalsScored = goalsScored;
+        this.shirtNumber = shirtNumber;
         this.fieldPosition = fieldPosition;
     }
+    public void getFootballerDetails() {
+        System.out.println("=================================");
+        System.out.println("Player name: " + this.playerName);
+        System.out.println("Player surname: " + this.playerSurname);
+        System.out.println("Sport: " + this.playerSport);
+        System.out.println("Team: " + this.playerTeam);
+        System.out.println("League: " + this.league);
+        System.out.println("Goals scored: " + this.goalsScored);
+        System.out.println("Shirt Number: " + this.shirtNumber);
+        System.out.println("Position on the field: " + this.fieldPosition);
+        System.out.println("=================================");
+    }
 }
-class CricketStats extends PlayerStats{
+class BasketballStats extends PlayerStats{
     public int pointsScored;
     public String league;
     public float playerShoeSize;
     public double playerHeight;
 
-    public CricketStats(String playerName, String playerSurname, String playerSport, String playerTeam,
+    public BasketballStats(String playerName, String playerSurname, String playerSport, String playerTeam,
                          int pointsScored, String league, float playerShoeSize, double playerHeight) {
         super(playerName, playerSurname, playerSport, playerTeam);
 
@@ -40,8 +54,25 @@ class CricketStats extends PlayerStats{
         this.playerHeight = playerHeight;
     }
 }
-class SportReport {
-    public static void main (String[] args) {
+class CricketStats extends PlayerStats{
+    public int runsScored;
+    public int catches;
+    public int wicketsTaken;
+    public int strikeRate;
+    public CricketStats(String playerName, String playerSurname, String playerSport, String playerTeam,
+                         int runsScored, int catches, int wicketsTaken, int strikeRate) {
+        super(playerName, playerSurname, playerSport, playerTeam);
+        this.runsScored = runsScored;
+        this.catches = catches;
+        this.wicketsTaken = wicketsTaken;
+        this.strikeRate = strikeRate;
 
     }
 }
+
+class SportReport {
+    public static void main(String[] args) {
+
+    }
+}
+
