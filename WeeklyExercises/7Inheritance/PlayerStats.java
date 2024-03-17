41e6c65ee54d196e98e3bedfb0e882d01d545a45
imposible normalito <imposiblenormalito@gmail.com>
2024-03-17 18:58:09 +0000

    created instances of the subclasses

--- WeeklyExercises/7Inheritance/PlayerStats.java
+++ WeeklyExercises/7Inheritance/PlayerStats.java
@@ -1,13 +1,11 @@
 abstract class PlayerStats{
     public String playerName;
     public String playerSurname;
-    public String playerSport;
     public String playerTeam;
 
-    public PlayerStats(String playerName, String playerSurname, String playerSport, String playerTeam) {
+    public PlayerStats(String playerName, String playerSurname, String playerTeam) {
         this.playerName = playerName;
         this.playerSurname = playerSurname;
-        this.playerSport = playerSport;
         this.playerTeam = playerTeam;
     }
 }
@@ -16,9 +14,9 @@ class FootballStats extends PlayerStats{
     public int goalsScored;
     public int shirtNumber;
     public String fieldPosition;
-    public FootballStats(String playerName, String playerSurname, String playerSport, String playerTeam,
+    public FootballStats(String playerName, String playerSurname, String playerTeam,
                          int goalsScored, int shirtNumber, String league, String fieldPosition) {
-        super(playerName, playerSurname, playerSport, playerTeam);
+        super(playerName, playerSurname, playerTeam);
 
         this.league = league;
         this.goalsScored = goalsScored;
@@ -29,7 +27,6 @@ class FootballStats extends PlayerStats{
         System.out.println("=================================");
         System.out.println("Player name: " + this.playerName);
         System.out.println("Player surname: " + this.playerSurname);
-        System.out.println("Sport: " + this.playerSport);
         System.out.println("Team: " + this.playerTeam);
         System.out.println("League: " + this.league);
         System.out.println("Goals scored: " + this.goalsScored);
@@ -44,14 +41,25 @@ class BasketballStats extends PlayerStats{
     public float playerShoeSize;
     public double playerHeight;
 
-    public BasketballStats(String playerName, String playerSurname, String playerSport, String playerTeam,
+    public BasketballStats(String playerName, String playerSurname, String playerTeam,
                          int pointsScored, String league, float playerShoeSize, double playerHeight) {
-        super(playerName, playerSurname, playerSport, playerTeam);
+        super(playerName, playerSurname, playerTeam);
 
         this.pointsScored = pointsScored;
         this.league = league;
         this.playerShoeSize = playerShoeSize;
         this.playerHeight = playerHeight;
+    }
+    public void getBasketballerDetails() {
+        System.out.println("=================================");
+        System.out.println("Player name: " + this.playerName);
+        System.out.println("Player surname: " + this.playerSurname);
+        System.out.println("Team: " + this.playerTeam);
+        System.out.println("League: " + this.league);
+        System.out.println("Goals scored: " + this.pointsScored);
+        System.out.println("Shoe size: " + this.playerShoeSize);
+        System.out.println("Height: " + this.playerHeight);
+        System.out.println("=================================");
     }
 }
 class CricketStats extends PlayerStats{
@@ -59,20 +67,38 @@ class CricketStats extends PlayerStats{
     public int catches;
     public int wicketsTaken;
     public int strikeRate;
-    public CricketStats(String playerName, String playerSurname, String playerSport, String playerTeam,
-                         int runsScored, int catches, int wicketsTaken, int strikeRate) {
-        super(playerName, playerSurname, playerSport, playerTeam);
+    public CricketStats(String playerName, String playerSurname, String playerTeam,
+                        int runsScored, int catches, int wicketsTaken, int strikeRate) {
+        super(playerName, playerSurname, playerTeam);
         this.runsScored = runsScored;
         this.catches = catches;
         this.wicketsTaken = wicketsTaken;
         this.strikeRate = strikeRate;
 
     }
+    public void getCricketerDetails() {
+        System.out.println("=================================");
+        System.out.println("Player name: " + this.playerName);
+        System.out.println("Player surname: " + this.playerSurname);
+        System.out.println("Team: " + this.playerTeam);
+        System.out.println("Runs scored: " + this.runsScored);
+        System.out.println("Catches: " + this.catches);
+        System.out.println("Wickets taken: " + this.wicketsTaken);
+        System.out.println("Strike rate: " + this.strikeRate);
+        System.out.println("=================================");
+    }
 }
 
 class SportReport {
     public static void main(String[] args) {
+        CricketStats cricketPlayer1 = new CricketStats("Aaron","Skyes","Chicago",8,5,8,8);
+        cricketPlayer1.getCricketerDetails();
 
+        BasketballStats basketball1 = new BasketballStats("LeBron","James","Lakers",75,"NBA",9,1.80);
+        basketball1.getBasketballerDetails();
+
+        FootballStats footballer1 = new FootballStats("Andres","Iniesta","Spain",42,8,"La Liga","Midfielder");
+        footballer1.getFootballerDetails();
     }
 }
 
