5e09e36d98d154a54978334fbcc6eddef9119cc8
LGandia <imposiblenormalito@gmail.com>
2024-03-19 13:57:49 +0000

    copied the repo files

--- WeeklyExercises/8Polymorphism/Encryptable.java
+++ WeeklyExercises/8Polymorphism/Encryptable.java
@@ -1,2 +1,12 @@
-public class Encryptable {
+//********************************************************************
+//  Encryptable.java       Java Foundations
+//
+//  Represents the interface for an object that can be encrypted
+//  and decrypted.
+//********************************************************************
+
+public interface Encryptable
+{
+    public void encrypt();
+    public String decrypt();
 }
