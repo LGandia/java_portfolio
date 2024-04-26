7376c6ff44d488f5e0a17eec3ac4f0b7f4b83768
imposible normalito <imposiblenormalito@gmail.com>
2024-04-26 15:53:36 +0100

    create turtlegraphics file that extends oopgraphics

---
+++ AssignmentTurtleGraphicsProgram/TurtleGraphics.java
@@ -0,0 +1,9 @@
+import java.awt.FlowLayout;
+import javax.swing.JFrame;
+import uk.ac.leedsbeckett.oop.OOPGraphics;
+public class TurtleGraphics extends OOPGraphics{
+    @Override
+    public void processCommand(String s) {
+
+    }
+}
