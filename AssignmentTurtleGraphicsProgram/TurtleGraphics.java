6730fc86a36f7cf4408172ceae3871ff6d42b80f
imposible normalito <imposiblenormalito@gmail.com>
2024-04-30 20:27:08 +0100

    added comfirmation and saved feature

--- AssignmentTurtleGraphicsProgram/TurtleGraphics.java
+++ AssignmentTurtleGraphicsProgram/TurtleGraphics.java
@@ -1,4 +1,6 @@
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -8,12 +10,12 @@ import javax.swing.*;
 
 import uk.ac.leedsbeckett.oop.OOPGraphics;
 public class TurtleGraphics extends OOPGraphics{
+    public static boolean saved = false;
     public static void main(String[] args)
     {
         new TurtleGraphics();
     }
-    public TurtleGraphics()
-    {
+    public TurtleGraphics() {
         JFrame Screen = new JFrame();
         Screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Screen.setLayout(new FlowLayout());
@@ -21,16 +23,40 @@ public class TurtleGraphics extends OOPGraphics{
         Screen.pack();
         Screen.setVisible(true);
         penDown();
+
+        Screen.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        Screen.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                if (!saved) {
+                    int confirm = JOptionPane.showConfirmDialog(Screen, "You have not saved your image. Are you sure you want to close this window?",
+                            "Confirm", JOptionPane.YES_NO_OPTION);
+                    if (confirm == JOptionPane.YES_OPTION) {
+                        System.exit(0);
+                    }else {
+                        processCommand("save");
+                    }
+
+                }else {
+                    int confirm = JOptionPane.showConfirmDialog(Screen, "Are you sure you want to close this window?",
+                            "Confirm", JOptionPane.YES_NO_OPTION);
+                    if (confirm == JOptionPane.YES_OPTION) {
+                        System.exit(0);
+                    }
+                }
+            }
+        });
     }
 
     public void processCommand(String command) {
+        System.out.println(saved);
         int countWords = command.split("\\s").length;
-        boolean saved = false;
 
         if (countWords == 1){
             switch (command) {
                 case "about":
                     about();
+                    saved = false;
                     break;
                 case "load":
                     System.out.println("load");
@@ -46,7 +72,6 @@ public class TurtleGraphics extends OOPGraphics{
                         while (outputfile.exists()){
                             int response = JOptionPane.showConfirmDialog(null, "The image file " + imageName +
                                     " already exists. Do you want to overwrite it?", "Confirm", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
-                            //JOptionPane.showMessageDialog(null,imageName + " already exists");
                             if (response == JOptionPane.YES_OPTION){
                                 break;
                             }else {
@@ -65,7 +90,16 @@ public class TurtleGraphics extends OOPGraphics{
                     reset();
                     break;
                 case "clear":
-                    clear();
+                    if(!saved){
+                        int response = JOptionPane.showConfirmDialog(null, "You have not saved your image. " +
+                                "Do you wish to reset without saving?", "Confirm", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+                        if (response == JOptionPane.YES_OPTION){
+                            clear();
+                            break;
+                        }else {
+                            processCommand("save");
+                        }
+                    }
                     break;
 
                 case "penup":
@@ -80,19 +114,19 @@ public class TurtleGraphics extends OOPGraphics{
 
                 case "black":
                     setPenColour(Color.black);
-                    System.out.println("black");
+                    saved = false;
                     break;
                case "green":
                    setPenColour(Color.green);
-                   System.out.println("green");
+                   saved = false;
                    break;
                case "red":
                    setPenColour(Color.red);
-                   System.out.println("red");
+                   saved = false;
                    break;
                case "white":
                    setPenColour(Color.white);
-                   System.out.println("white");
+                   saved = false;
                    break;
 
                case "turnleft", "turnright", "forward", "backward":
@@ -117,18 +151,19 @@ public class TurtleGraphics extends OOPGraphics{
                     switch (words[0]) {
                         case "turnleft":
                             turnLeft(amount);
-                            System.out.println("You want to turn left");
+                            saved = false;
                             break;
                         case "turnright":
                             turnRight(amount);
-                            System.out.println("You want to turn right");
+                            saved = false;
                             break;
                         case "forward":
                             forward(amount);
+                            saved = false;
                             break;
                         case "backward":
                             forward(-amount);
-                            System.out.println("You want to go backwards");
+                            saved = false;
                             break;
                         default:
                             System.out.println("Invalid command");
